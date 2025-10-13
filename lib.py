18586ad618ba382cfa88bc0477fa771cd386efb7
Aashaka-Pundir <aashakapundir2384.ap@gmail.com>
2025-10-13 11:57:50 +0530

    Update lib.py

--- lib.py
+++ lib.py
@@ -1,16 +1,31 @@
-from django import forms
-from .models import Story
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+def register():
+    username = input("Enter new username: ")
+    password = input("Enter new password: ")
 
-class StoryForm(forms.ModelForm):
-    class Meta:
-        model = Story
-        fields = ['title', 'content', 'category', 'published']
+    with open("users.txt", "a") as file:
+        file.write(f"{username},{password}\n")
+    print("Registration successful!")
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+def login():
+    username = input("Enter username: ")
+    password = input("Enter password: ")
 
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+    with open("users.txt", "r") as file:
+        for line in file:
+            stored_user, stored_pass = line.strip().split(",")
+            if stored_user == username and stored_pass == password:
+                print("Login successful!")
+                return
+    print("Invalid credentials.")
+
+while True:
+    choice = int(input("Choose 1-'register' \n 2-'login' \n 3-'exit': "))
+    if choice == 1:
+        register()
+    elif choice == 2:
+        login()
+    elif choice==3:
+        print("Exiting..")
+        break
+    else:
+        print("Invalid choice.")
